c34188324ed86bba466637e215312dadd0e5e5b6
Ptashkina Liliya <efekta9@gmail.com>
2019-02-14 18:26:53 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,18 @@
 ;$(document).ready(function() {
 		ymaps.ready(function(){
-	    // Указывается идентификатор HTML-элемента.
+			
 	    var myMap = new ymaps.Map("my_map", {
 	        center: [51.64366779, 38.95649378],
 	        zoom: 15
 	    });
-	    // var myPlacemark = new ymaps.Placemark([51.64523849, 38.95769479], {
-	    // 	balloonContentHeader: '<img src="icons/placeholder@2x.png">'
-	    // });
 	    var myPlacemark = new ymaps.Placemark([51.64409499, 38.96516268], {}, {
     	iconLayout: 'default#image',
-    	// iconImageClipRect: [[0,0], [26, 46]],
     	iconImageHref: 'icons/placeholder@2x.png',
     	iconImageSize: [38, 54]
-    	// iconImageOffset: [-15, -27]
 	});
 		myMap.geoObjects.add(myPlacemark);
 		myMap.behaviors.disable(['scrollZoom']);
 	});
-  // ymaps.ready(init);
-  // var myMap;
-
-  // function init(){
-  //     myMap = new ymaps.Map ("my_map", {
-  //         center: [51.64523849, 38.95769479],
-  //         zoom: 13
-  //     });
-
-  //     myMap.controls.remove('searchControl').remove('trafficControl').remove('geolocationControl');
-
-  //     myMap.behaviors.disable(['scrollZoom']);
-
-
-  //     myPlacemark1 = new ymaps.Placemark([51.64523849, 38.95769479], {
-  //       balloonContentHeader: '<img src="icons/placeholder.svg" alt="Кликай, не стенсняйся :)" title="Кликай, не стенсняйся :)" class="mc__logo"><span class="mc__home">Штаб Master-CSS</span>',
-  //       balloonContentBody: 'Здесь живу я и моя собака - Алиса',
-  //       balloonContentFooter: '<span class="mc__foot">каждому по комнате</span>',
-  //       hintContent: 'Мой дом'
-  //     });
-
-      // myPin.add(myPlacemark1);
-
-      // myMap.geoObjects.add(myPin);
-  // }
-
 });
 
 
